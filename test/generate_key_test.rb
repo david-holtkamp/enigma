6c3a43352ef07263c3a4a52b3ee478ae5f758f6e
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-11 15:47:29 -0700

    Write tests for generating random key and class method to generate key

---
+++ test/generate_key_test.rb
@@ -0,0 +1,19 @@
+require_relative 'test_helper.rb'
+require './lib/generate_key'
+
+class GenerateKeyTest < Minitest::Test
+
+  def test_it_can_generate_random_number
+    GenerateKey.stubs(:random_key).returns("12345")
+
+    assert_equal "12345", GenerateKey.random_key
+  end
+
+  def test_it_returns_string
+    assert_instance_of String, GenerateKey.random_key
+  end
+
+  def test_it_returns_random_number_of_proper_length
+    assert_equal 5, GenerateKey.random_key.length
+  end
+end
