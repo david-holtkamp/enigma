a929b2d5cb14942ba7ec45267949e796be269960
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-12 09:51:23 -0700

    Refactor generate key test to use a stub and not pass key as argument

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,31 +5,52 @@ require './lib/generate_date'
 
 class ShiftTest < Minitest::Test
 
-  def setup
-    @shift = Shift.new
-  end
+  # def setup
+  #   @shift = Shift.new
+  # end
 
   def test_it_exists
-    assert_instance_of Shift, @shift
+    shift = Shift.new
+
+    assert_instance_of Shift, shift
+  end
+
+  def test_it_initializes
+    shift = Shift.new
+
+    assert_instance_of String, shift.key
+    assert_equal 5, shift.key.length
+
+    assert_instance_of String, shift.date
+    assert_equal 6, shift.date.length
   end
 
   def test_key_shift
-    assert_equal [12, 23, 34, 45], @shift.create_key("12345")
-    assert_instance_of Array, @shift.create_key("12345")
+    shift = Shift.new
+
+    shift.stubs(:key => "12345")
+    expected = [12, 23, 34, 45]
+
+    assert_equal expected, shift.create_key
+    assert_instance_of Array, shift.create_key
   end
 
   def test_date_offset
-    assert_instance_of Array, @shift.offset("110120")
-    assert_equal 4, @shift.offset("110120").first
-    assert_equal 4, @shift.offset("110120")[1]
-    assert_equal 0, @shift.offset("110120")[2]
-    assert_equal 0, @shift.offset("110120").last
-    assert_equal [4, 4, 0, 0], @shift.offset("110120")
+    shift = Shift.new
+
+    assert_instance_of Array, shift.offset("110120")
+    assert_equal 4, shift.offset("110120").first
+    assert_equal 4, shift.offset("110120")[1]
+    assert_equal 0, shift.offset("110120")[2]
+    assert_equal 0, shift.offset("110120").last
+    assert_equal [4, 4, 0, 0], shift.offset("110120")
   end
 
   def test_it_can_shift
-    @shift.create_key("12345")
-    @shift.offset("110120")
-    assert_equal [16, 27, 34, 45], @shift.shift("12345", "110120")
+    shift = Shift.new
+
+    shift.create_key
+    shift.offset("110120")
+    assert_equal [16, 27, 34, 45], shift.shift("12345", "110120")
   end
 end
