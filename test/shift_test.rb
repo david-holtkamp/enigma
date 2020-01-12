1830c69babd5a460ca84df2252cb9415b7c1c0ef
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-12 10:24:31 -0700

    Clean up test and use setup and in instance variable

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,56 +5,45 @@ require './lib/generate_date'
 
 class ShiftTest < Minitest::Test
 
-  # def setup
-  #   @shift = Shift.new
-  # end
+  def setup
+    @shift = Shift.new
+  end
 
   def test_it_exists
-    shift = Shift.new
-
-    assert_instance_of Shift, shift
+    assert_instance_of Shift, @shift
   end
 
   def test_it_initializes
-    shift = Shift.new
+    assert_instance_of String, @shift.key
+    assert_equal 5, @shift.key.length
 
-    assert_instance_of String, shift.key
-    assert_equal 5, shift.key.length
-
-    assert_instance_of String, shift.date
-    assert_equal 6, shift.date.length
+    assert_instance_of String, @shift.date
+    assert_equal 6, @shift.date.length
   end
 
   def test_key_shift
-    shift = Shift.new
-
-    shift.stubs(:key => "12345")
+    @shift.stubs(:key => "12345")
     expected = [12, 23, 34, 45]
 
-    assert_equal expected, shift.create_key
-    assert_instance_of Array, shift.create_key
+    assert_equal expected, @shift.create_key
+    assert_instance_of Array, @shift.create_key
   end
 
   def test_date_offset
-    shift = Shift.new
-
-    shift.stubs(:date => "110120")
+    @shift.stubs(:date => "110120")
     expected = [4, 4, 0, 0]
-    assert_instance_of Array, shift.offset
-    assert_equal 4, shift.offset.first
-    assert_equal 4, shift.offset[1]
-    assert_equal 0, shift.offset[2]
-    assert_equal 0, shift.offset.last
-    assert_equal expected, shift.offset
+    assert_instance_of Array, @shift.offset
+    assert_equal 4, @shift.offset.first
+    assert_equal 4, @shift.offset[1]
+    assert_equal 0, @shift.offset[2]
+    assert_equal 0, @shift.offset.last
+    assert_equal expected, @shift.offset
   end
 
   def test_it_can_shift
-    shift = Shift.new
+    @shift.stubs(:key => "12345")
+    @shift.stubs(:date => "110120")
 
-    shift.stubs(:key => "12345")
-    shift.stubs(:date => "110120")
-
-
-    assert_equal [16, 27, 34, 45], shift.shift
+    assert_equal [16, 27, 34, 45], @shift.shift
   end
 end
