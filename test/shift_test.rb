3dda997ba07b000bdb45359773967907f37ec8e8
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-11 21:13:19 -0700

    Write tests for taking date and creating offset method

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -17,4 +17,12 @@ class ShiftTest < Minitest::Test
     assert_equal [12, 23, 34, 45], @shift.create_key("12345")
     assert_instance_of Array, @shift.create_key("12345")
   end
+
+  def test_date_offset
+    assert_instance_of Array, @shift.offset("110120")
+    assert_equal [4], @shift.offset("110120").first
+    assert_equal [4], @shift.offset("110120")[1]
+    assert_equal [0], @shift.offset("110120")[2]
+    assert_equal [0], @shift.offset("110120").last 
+  end
 end
