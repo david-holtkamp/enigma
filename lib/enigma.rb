e804a16035378d1de46c7865f901557c75049b98
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-13 21:39:28 -0700

    Bring shift generate shift method into enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,13 @@
+require_relative 'shift'
+
 class Enigma
-  attr_reader :key, :date, :alphabet
+  attr_reader :key, :date, :alphabet, :shift
 
   def initialize
     @key = GenerateKey.random_key
     @date = GenerateDate.generate_date
     @alphabet = ("a".."z").to_a << " "
+    @shift = Shift.generate_shift(key = @key, date = @date)
+    require "pry"; binding.pry
   end
-
-
 end
