f11b4ec1a5b8e9b798708870382277cc8d82c185
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-13 21:20:32 -0700

    Initialize key and date inst variables by calling class methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,11 @@
-class Enigma 
-  attr_reader :message, :key, :date, :alphabet
+class Enigma
+  attr_reader :key, :date, :alphabet
 
   def initialize
-    @key = key
-    @date = date
+    @key = GenerateKey.random_key
+    @date = GenerateDate.generate_date
     @alphabet = ("a".."z").to_a << " "
   end
+
+
 end
