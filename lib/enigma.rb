af473ad6a95871bd1d0eff7f81a32f5954cedc6f
David Holtkamp <david.hholtkamp@gmail.com>
2020-01-14 16:02:04 -0700

    Write method for rotating chunk of message using shift method as arg

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,6 @@
 require_relative 'shift'
+require_relative 'generate_key'
+require_relative 'generate_date'
 
 class Enigma
   attr_reader :key, :date, :alphabet, :shift
@@ -7,10 +9,24 @@ class Enigma
     @key = GenerateKey.random_key
     @date = GenerateDate.generate_date
     @alphabet = ("a".."z").to_a << " "
-    @shift = Shift.generate_shift(key = @key, date = @date)
+    # @shift = []
   end
+
+  # def final_shift
+  #   @shift = Shift.generate_shift(key, date)
+  # end
 
   def message_chunked(message)
     message.downcase.chars.each_slice(4).to_a
+  end
+
+  def rotate_chunk(chunk, final_shift)
+    chunk.map.with_index do |letter, index|
+      if @alphabet.include?(letter)
+        @alphabet.rotate(@alphabet.index(letter) +final_shift[index]).first 
+      else
+        letter
+      end
+    end
   end
 end
